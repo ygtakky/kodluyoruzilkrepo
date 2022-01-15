78c415d3371cf290e72aa31e96d57d95e9184977
ygtakky <ygtakky@gmail.com>
2022-01-15 17:49:43 +0300

    v1 Dosyalar

---
+++ Ödevler/todolist/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+  localStorage.setItem("tasks", JSON.stringify(tasksArray));
+  const data = JSON.parse(localStorage.getItem("tasks"));
+  loadTasks(data);
+});
+
+const list = document.querySelector("#list");
+
+let tasksArray = localStorage.getItem("tasks")
+    ? JSON.parse(localStorage.getItem("tasks"))
+    : [];
+
+function loadTasks(data) {
+  data.forEach((task) => {
+    liMaker(task);
+  });
+}
+
+function liMaker(val) {
+  const li = `<li>${val}<span class="delete" onclick="deleteElement(event);">&#10060</span></li>`;
+  if (list.innerHTML == "") {
+    list.innerHTML = li;
+  } else {
+    list.innerHTML += li;
+  }
+}
+
+function newElement() {
+  const task = $("#task");
+  let taskVal = task.val().trim();
+  if (taskVal === "") {
+    alert("Yapılacak boş olamaz!");
+    return 0;
+  }
+  tasksArray.push(taskVal);
+  localStorage.setItem("tasks", JSON.stringify(tasksArray));
+  liMaker(taskVal);
+  taskVal = "";
+}
+
+function deleteElement(event) {
+  const task = event.target.parentElement.innerText.slice(0, -1);
+  tasksArray.pop(task);
+  localStorage.setItem("tasks", JSON.stringify(tasksArray));
+  event.target.parentElement.remove();
+}
+
+function clearAll() {
+  localStorage.clear();
+}
